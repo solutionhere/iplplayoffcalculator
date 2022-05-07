78c2fee520229a78f357412a11f95656b070564b
AnandDev24 <anandmoorthymails@gmail.com>
2022-05-07 21:21:27 +0530

    imgs changed

--- src/app/ipl/util.ts
+++ src/app/ipl/util.ts
@@ -1,12 +1,12 @@
 export const logos: any = {
-    "Lucknow Super Giants": "/assets/ipllogos/LSGroundbig.png",
-    "Gujarat Titans": "/assets/ipllogos/GTroundbig.png",
-    "Royal Challengers Bangalore": "/assets/ipllogos/RCBroundbig.png",
-    "Kolkata Knight Riders": "/assets/ipllogos/KKRroundbig.png",
-    "Punjab Kings": "/assets/ipllogos/PBKSroundbig.png",
-    "Chennai Super Kings":"/assets/ipllogos/CSKroundbig.png",
-    "Delhi Capitals": "/assets/ipllogos/DCroundbig.png",
-    "Rajasthan Royals": "/assets/ipllogos/RRroundbig.png",
-    "Mumbai Indians": "/assets/ipllogos/MIroundbig.png",
-    "Sunrisers Hyderabad": "/assets/ipllogos/SRHroundbig.png"
+    "Lucknow Super Giants": "assets/ipllogos/LSGroundbig.png",
+    "Gujarat Titans": "assets/ipllogos/GTroundbig.png",
+    "Royal Challengers Bangalore": "assets/ipllogos/RCBroundbig.png",
+    "Kolkata Knight Riders": "assets/ipllogos/KKRroundbig.png",
+    "Punjab Kings": "assets/ipllogos/PBKSroundbig.png",
+    "Chennai Super Kings":"assets/ipllogos/CSKroundbig.png",
+    "Delhi Capitals": "assets/ipllogos/DCroundbig.png",
+    "Rajasthan Royals": "assets/ipllogos/RRroundbig.png",
+    "Mumbai Indians": "assets/ipllogos/MIroundbig.png",
+    "Sunrisers Hyderabad": "assets/ipllogos/SRHroundbig.png"
 }
